9f173cfa676430741ce9a558f47eb8d6a0a89c33
kzlim5678-design <kzlim5678@gmail.com>
2026-09-10 11:46:32 +0800

    Update User.java

--- src/apu_asc/model/User.java
+++ src/apu_asc/model/User.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package apu_asc.model;
 
-/**
- *
- * @author User
- */
+
 public class User {
     
     private String userID;
@@ -25,8 +18,45 @@ public class User {
         this.phoneNumber = phoneNumber;
         this.role = role;
     }
-    
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     public String getName() {
         return name;
     }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
 }
